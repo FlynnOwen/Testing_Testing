df58da2ed3de3f92b502530e76f2e688ca62a111
Flynn Owen <flynnoo49@gmail.com>
2022-05-24 22:49:49 +1200

    add docs to mock

--- Tests/test_monkeypatch_add.py
+++ Tests/test_monkeypatch_add.py
@@ -7,6 +7,7 @@ def test_add_monkeypatch(monkeypatch):
     def mockreturn(x, y):
         return 10
 
+    # From module add, we wish to patch function "add" with the result from mockreturn
     monkeypatch.setattr(add, "add", mockreturn)
 
     result = add.add(1,3)
