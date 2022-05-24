81942ac64ac73c78835d99cdebc52c70a325fa21
Flynn Owen <flynnoo49@gmail.com>
2022-05-24 22:49:03 +1200

    add docs to monkeypatch

--- Tests/test_add.py
+++ Tests/test_add.py
@@ -1,5 +1,11 @@
 import pytest
 from src.add import add
 
-def test_add():
-	assert 5 == add(2,3)
+
+# We can mark a set of values to test - and whether any of them should fail
+@pytest.mark.parametrize(
+	"test_input, expected",
+	[((2,3), 5), ((3,5), 8), pytest.param((100,100), 20, marks=pytest.mark.xfail)]
+)
+def test_add(test_input, expected):
+	assert add(*test_input) == expected
